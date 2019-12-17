3a800cb698bb60b119f85c7aa714c15838c02271
osmelsr97@gmail.com <osmelsr97@gmail.com>
2019-12-17 17:04:11 -0500

    Step3

--- 2019/Diciembre/Osmel/DeviceAuthenticator/index.js
+++ 2019/Diciembre/Osmel/DeviceAuthenticator/index.js
@@ -1,30 +1,35 @@
 var express = require('express');
 var http = require('http');
-var app = express();
+var path = require('path');
+var body_parser = require('body-parser');
 const fs = require('fs');
+var app = express();
+const NodeRSA = require('node-rsa');
+const key = new NodeRSA({ b:512 });
+
+app.use(body_parser.urlencoded({ extended: true }));
 
 app.post('/server/getIdentity', (req, res) => {
-    // var user_id = require('./identity/server.id');
+    var user_id = require('./identity/server.id');
     res.status(200).send(user_id);
 });
 
 app.post('/devices/createSession', (req, res) => {
     var deviceIdentities = require('./libs/DeviceIdentitiesClient/DeviceIdentities');
+    const pvtKey = fs.readFileSync(path.resolve('./keys/pvt.key'));
 
-    const data = JSON.stringify({
-        dev_serial: req.query.dev_serial,
-        dev_identity: req.query.dev_identity,
-        secured: req.query.secured,
-        payload: req.query.payload
-    });
+    var dev_serial = req.body.dev_serial;
+    var dev_identity = req.body.dev_identity;
+    var secured = req.body.secured;
+    var payload = req.body.payload;
 
     verifyIdentity = deviceIdentities.verify(data.dev_serial, data.dev_identity);
     if (verifyIdentity == 'failed') {
         res.status(403).send('Error');
-    }  else if(verifyIdentity == 'success') {
-        payload_json = RSA_DECRYPT(data.payload);
-        fs.writeFileSync('payload_json.skey', payload_json);
-        res.status(200).send('createSession');
+    } else if (verifyIdentity == 'success') {
+    var payload_json = key.decrypt(payload, pvtKey);
+
+    res.status(200).send(payload_json);
     }
 });
 
